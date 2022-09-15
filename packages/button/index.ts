21794a5971efff7ffbfd8555639d12c7745eb1c6
decadehew <yong_323@hotmail.com>
2022-09-16 01:15:15 +0800

    chore: add commit cz && vue-shim.d.ts

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -1,5 +1,6 @@
 import { App } from 'vue'
 import Button from './src/index.vue'
-export const install = function (app: App) {
+
+export default (app: App) => {
   app.component(Button.name, Button)
 }
--- src/component.ts
+++ src/component.ts
@@ -1,4 +1,4 @@
-import { install } from './components/button/index'
+import ButtonInstall from '@toy-ui/button/index'
 export const installAll = (app) => {
-  install(app)
+  ButtonInstall(app)
 }
