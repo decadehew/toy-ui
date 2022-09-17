adbb400701c682afb06d3757230e9b773c47a936
decadehew <yong_323@hotmail.com>
2022-09-17 13:44:29 +0800

    feat: add badge component

---
+++ packages/badge/index.ts
@@ -0,0 +1,6 @@
+import type { App } from 'vue'
+import Badge from './src/index.vue'
+
+export default (app: App) => {
+  app.component(Badge.name, Badge)
+}
--- packages/button/doc/index.stories.js
+++ packages/button/doc/index.stories.js
@@ -1,19 +1,24 @@
 import ElButton from '../index'
 
 export default {
-  title: 'Button'
+  title: 'Button',
+  component: ElButton
 }
 
 export const NormalButton = () => {
-  return {
-    template: '<el-button>With Text</el-button>',
-    installer: ElButton
-  }
+  // return {
+  //   template: '<el-button>With Text</el-button>',
+  //   installer: ElButton
+  // }
+
+  return '<el-button>With Text</el-button>'
 }
 
 export const ButtonTwo = () => {
-  return {
-    template: '<el-button>button two</el-button>',
-    installer: ElButton
-  }
+  // return {
+  //   template: '<el-button>button two</el-button>',
+  //   installer: ElButton
+  // }
+
+  return '<el-button>button two</el-button>'
 }
