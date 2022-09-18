f0bf1200b2b5f89381a90e5c6749c178d367a405
decadehew <yong_323@hotmail.com>
2022-09-18 19:37:54 +0800

    test: button component

--- packages/button/__tests__/button.spec.ts
+++ packages/button/__tests__/button.spec.ts
@@ -12,25 +12,117 @@ const COMMON_CONFIG = {
 }
 
 describe('Button.vue', () => {
+  it('create', () => {
+    const wrapper = mount(Button, {
+      props: { type: 'primary' },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('el-button--primary')
+  })
+
+  it('icon', () => {
+    const wrapper = mount(Button, {
+      props: { icon: 'el-icon-search' },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.find('.el-icon-search').exists()).toBeTruthy()
+  })
+  it('nativeType', () => {
+    const wrapper = mount(Button, {
+      props: { nativeType: 'submit' },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.attributes('type')).toBe('submit')
+  })
+  it('loading', () => {
+    const wrapper = mount(Button, {
+      props: { loading: true },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('is-loading')
+    expect(wrapper.find('.el-icon-loading').exists()).toBeTruthy()
+  })
+  it('size', () => {
+    const wrapper = mount(Button, {
+      props: { size: 'medium' },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('el-button--medium')
+  })
+  it('plain', () => {
+    const wrapper = mount(Button, {
+      props: { plain: true },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('is-plain')
+  })
+  it('round', () => {
+    const wrapper = mount(Button, {
+      props: { round: true },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('is-round')
+  })
+  it('circle', () => {
+    const wrapper = mount(Button, {
+      props: { circle: true },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('is-circle')
+  })
+
   test('render text', () => {
-    const instance = mount(Button, {
+    const wrapper = mount(Button, {
       slots: {
         default: AXIOM,
       },
       ...COMMON_CONFIG,
     })
-    expect(instance.text()).toEqual(AXIOM)
+    expect(wrapper.text()).toEqual(AXIOM)
   })
 
   test('handle click', async () => {
-    const instance = mount(Button, {
+    const wrapper = mount(Button, {
       slots: {
         default: AXIOM,
       },
       ...COMMON_CONFIG,
     })
+    await wrapper.trigger('click')
+    expect(wrapper.emitted()).toBeDefined()
 
-    await instance.trigger('click')
-    expect(instance.emitted()).toBeDefined()
+  })
+
+  test('handle click inside', async () => {
+    const wrapper = mount(Button, {
+      slots: {
+        default: '<span class="inner-slot"></span>',
+      },
+      ...COMMON_CONFIG,
+    })
+    await (<HTMLElement>wrapper.element.querySelector('.inner-slot')).click()
+    expect(wrapper.emitted()).toBeDefined()
+  })
+
+  test('loading implies disabled', async () => {
+    const wrapper = mount(Button, {
+      slots: {
+        default: AXIOM,
+      },
+      props: { loading: true },
+      ...COMMON_CONFIG,
+    })
+    await wrapper.trigger('click')
+    expect(wrapper.emitted('click')).toBeUndefined()
+  })
+
+  it('disabled', async () => {
+    const wrapper = mount(Button, {
+      props: { disabled: true },
+      ...COMMON_CONFIG,
+    })
+    expect(wrapper.classes()).toContain('is-disabled')
+    await wrapper.trigger('click')
+    expect(wrapper.emitted('click')).toBeUndefined()
   })
 })
