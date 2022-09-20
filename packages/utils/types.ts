a3b373eeb7923353d2eea569803b113094814839
decadehew <yong_323@hotmail.com>
2022-09-20 23:40:17 +0800

    chore: update utils/*

---
+++ packages/utils/types.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-types */
+type OptionalKeys<T extends object> = {
+  [K in keyof T]: T extends Record<K, T[K]>
+    ? never
+    : K
+}[keyof T]
+
+type RequiredKeys<T extends object> = Exclude<keyof T, OptionalKeys<T>>
+
+type MonoArgEmitter<T, Keys extends keyof T> = <K extends Keys>(evt: K, arg?: T[K]) => void
+
+type BiArgEmitter<T, Keys extends keyof T> = <K extends Keys>(evt: K, arg: T[K]) => void
+
+export type EventEmitter<T extends object> =
+  MonoArgEmitter<T, OptionalKeys<T>> & BiArgEmitter<T, RequiredKeys<T>>
+
+type A = Required<{}>
