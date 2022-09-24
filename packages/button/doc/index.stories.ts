19837af2f24db1d73bf43b02beda01ad54723267
decadehew <yong_323@hotmail.com>
2022-09-24 20:02:47 +0800

    feat: add button group component

--- packages/button/doc/index.stories.ts
+++ packages/button/doc/index.stories.ts
@@ -22,3 +22,8 @@ export const ButtonTwo = (): string => {
 
   return '<el-button>button two</el-button>'
 }
+
+export const ButtonGroup = (): string => `<el-button-group>
+<el-button type="primary" icon="el-icon-arrow-left">Left</el-button>
+<el-button type="primary">Right<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+</el-button-group>`
